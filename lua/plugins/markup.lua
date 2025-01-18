c5dbd0b7f82c78933b89783aa53bed66578a71a7
Spike <pastthescreen@protonmail.com>
2025-01-18 15:46:19 -0700

    Update config :3

--- lua/plugins/markup.lua
+++ lua/plugins/markup.lua
@@ -115,9 +115,6 @@ return {
 			vim.g.vimtex_view_method="general"
 			vim.g.vimtex_general_viewer = "okular"
 			-- vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
-			vim.g.vimtex_compiler_latexmk = {
-				executable = "clatexmk.sh"
-			}
 		end
 	},
 }
